0898929727d9b5fadf83c7c034dbba671ffbcd27
bthol <95544562+bthol@users.noreply.github.com>
2022-03-25 14:12:12 -0700

    install express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const mongoose = require(`mongoose`);
+const session = require('express-session')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
