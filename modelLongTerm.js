2b08c482497690108f10e9ad87fd8673a19c7983
bthol <95544562+bthol@users.noreply.github.com>
2022-03-25 12:26:29 -0700

    add second model

---
+++ modelLongTerm.js
@@ -0,0 +1,5 @@
+const dataStore = new Schema ({
+    dataDays: {dayNum: Number, projected: Number, completed: Number, rate: Number},
+    dataWeeks: {weekNum: Number, projected: Number, completed: Number, rate: Number},
+    dataMonths: {weekNum: Number, projected: Number, completed: Number, rate: Number}
+});
