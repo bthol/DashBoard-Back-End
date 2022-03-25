1551d7690a30eb56d231e90ee5e1b3520d27a97a
bthol <95544562+bthol@users.noreply.github.com>
2022-03-25 14:10:51 -0700

    code add

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,4 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 const UserModel = new Schema ({
     username: String,
-    password: String
-})
+    password: String,
+    log: {type: Boolean, default: false}
+}, {timestamps = true});
+
+const Recipe = mongoose.model('UserModel', UserModel);
+
+module.exports = UserModel
--- app.js
+++ app.js
@@ -5,12 +5,27 @@ const mongoose = require(`mongoose`);
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+}))
 
+//landing index
 app.get(`/`, (req, res) => {
     const ID = req;
     res.send("index is active")
 })
 
-module.export = app.listen(3000, () => {
+module.exports = (req, res, next)=>{
+    if(req.session.isLoggedIn){
+        next()
+    }else{
+        res.redirect("/users/login")
+    }
+}
+
+app.listen(3000, () => {
     console.log("express is online")
 });
