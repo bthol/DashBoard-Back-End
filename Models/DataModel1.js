811fa22abbcc9838be11cd28b616f4fd3df1215d
bthol <95544562+bthol@users.noreply.github.com>
2022-04-01 11:55:37 -0700

    schema reformat

--- Models/DataModel1.js
+++ Models/DataModel1.js
@@ -3,13 +3,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const daySchema = new Schema ([
-    {
+const daySchema = new Schema ({
     projected: {type: Number, default: 0},
     completed: {type: Number, default: 0},
     date: { type: Date, default: Date.now },
-    }
-]);
+}, {timeStamps: false});
 
 const DataModel1 = mongoose.model('DataModel1', daySchema);
 
@@ -17,12 +15,8 @@ module.exports = DataModel1
 
 //JSON format
 // {
-//     "projected": {
-//         "val":"0"
-//     },
-//     "completed": {
-//         "val":"0"
-//     }
+//     "projected": "0",
+//     "completed": "0"
 // }
 
 //////////////////////////////////////
