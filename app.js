5a665f916b22be1396bdb5dbeca932f56782b0c6
bthol <95544562+bthol@users.noreply.github.com>
2022-04-04 06:45:42 -0700

    update

--- app.js
+++ app.js
@@ -4,11 +4,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-var bodyParser = require('body-parser');
+const cors = require("cors")
+const bodyParser = require('body-parser');
 
 //Middleware
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
 //Controller indices
 const DataControl = require("./Controls/DataControl")
